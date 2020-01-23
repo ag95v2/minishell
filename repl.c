fbde69c789c398f20c1cd5474fcd6aaeb5ebd986
anton <goloviznin1995@yandex.ru>
2020-01-23 19:42:54 +0300

    Made pretty prompt

--- repl.c
+++ repl.c
@@ -3,11 +3,31 @@
 #include "find_exec.h"
 #include "expansions.h"
 #include "unistd.h"
+#include "limits.h"
+#include "colors.h"
 
 static void	prompt(void)
 {
+	static char	hostname[HOST_NAME_MAX];
+	static char	username[LOGIN_NAME_MAX];
+	static char	wd[PATH_MAX];
+	
 	if (isatty(0))
-		ft_putstr("minishell________$");
+	{
+		if (gethostname(hostname, HOST_NAME_MAX) != -1 &&\
+			getlogin_r(username, LOGIN_NAME_MAX) != -1)
+		{
+			ft_putstr(KGRN);
+			ft_printf("%s@%s:", username, hostname);
+		}
+		if (getcwd(wd, PATH_MAX))
+		{
+			ft_putstr(KRED);
+			ft_putstr(wd);
+		}
+		ft_putstr(KNRM);
+		ft_putstr("$ ");
+	}
 }
 
 int	run_cmd(t_env env, char **words, char **cmd)
