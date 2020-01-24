53a9bea037e697d4113270abf5681295b6990dd2
bgian <bgian@il-g6.21-school.ru>
2020-01-24 13:44:55 +0300

    Changed main() prototype to argc, argv, environ

--- minishell.c
+++ minishell.c
@@ -1,17 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgian <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/24 13:43:07 by bgian             #+#    #+#             */
+/*   Updated: 2020/01/24 13:43:46 by bgian            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "environment.h"
 #include "t_builtin.h"
 #include "repl.h"
 #include <signal.h>
 
-extern char	**environ;
-
 int			g_child;
 
 /*
-** Kill all children if they exist.
+** Kill (only) child if it exists.
 */
 
-void	handler(int s)
+void		handler(int s)
 {
 	(void)s;
 	kill(g_child, SIGINT);
@@ -20,13 +30,16 @@ void	handler(int s)
 /*
 ** sa_flags  = 0 means that we do not restart blocking syscalls in case of
 ** SIGINT. (gnl will fail with EINTR and return -1) =)
+** third parameter in main() is used  instead of extern char	**environ;
 */
 
-int	main()
+int			main(int argc, char **argv, char **environ)
 {
 	t_env				env;
 	struct sigaction	signal_action;
 
+	(void)argc;
+	(void)argv;
 	signal_action.sa_handler = handler;
 	sigemptyset(&signal_action.sa_mask);
 	signal_action.sa_flags = 0;
@@ -34,7 +47,6 @@ int	main()
 
 	if (!(env = init_env(environ)))
 	 	return (1);
-	//signal(SIGINT, handler);
 	repl(env);
 	delenv(env);
 	ft_putchar('\n');
--- to_array.c
+++ to_array.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   to_array.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgian <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/24 13:27:58 by bgian             #+#    #+#             */
+/*   Updated: 2020/01/24 13:29:25 by bgian            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "environment.h"
 
 static char		*concat_key_value(t_list *l)
@@ -49,7 +61,7 @@ static char		**list_to_array(t_list *l)
 
 static void		del_list_entry(void *content, size_t content_size)
 {
-	(void) content_size;
+	(void)content_size;
 	free(((t_key_value *)content)->key);
 	free(((t_key_value *)content)->value);
 	free(content);
@@ -57,7 +69,7 @@ static void		del_list_entry(void *content, size_t content_size)
 
 /*
 ** Ptree to char **
-** Flatten a prefix tree to NULL-terminated array of strings in 
+** Flatten a prefix tree to NULL-terminated array of strings in
 ** format "key=value"
 ** In case of memory errors, fill memory_ok with 0
 */
